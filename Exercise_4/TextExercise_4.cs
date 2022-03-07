09165185d80841d601ae15cb5f2ba5941f3a9196
Lohith Kumar Kommuru <82638777+lohith171@users.noreply.github.com>
2022-03-07 21:04:10 +0530

    Update TextExercise_4.cs

--- Exercise_4/TextExercise_4.cs
+++ Exercise_4/TextExercise_4.cs
@@ -1,4 +1,4 @@
-﻿using Moq;
+using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -18,8 +18,8 @@ namespace Exercise_4
             DbConnection obj = new SqlConnection("Sql ConnectionString");
             var stringWriter = new System.IO.StringWriter();
             Console.SetOut(stringWriter);
-            obj.Open("Sql");
-            Assert.AreEqual("Sql Connection is opened\r\n", stringWriter.ToString());
+            obj.Open();
+            Assert.AreEqual("DB Connection is opened\r\n", stringWriter.ToString());
         }
         [Test]
         public void ClosesDataBaseConnection()
@@ -27,19 +27,19 @@ namespace Exercise_4
             DbConnection obj = new OracleConnection("Sql ConnectionString");
             var stringWriter = new System.IO.StringWriter();
             Console.SetOut(stringWriter);
-            obj.Close("Oracle");
-            Assert.AreEqual("Oracle Connection is closed\r\n", stringWriter.ToString());
+            obj.Close();
+            Assert.AreEqual("DB Connection is closed\r\n", stringWriter.ToString());
         }
-        [TestCase("Oracle")]
-        [TestCase("Sql")]
-        public void ExecutesDataBaseCommand(string db)
+        [Test]
+      
+        public void ExecutesDataBaseCommand()
         {
             Mock<DbConnection> mockObj = new Mock<DbConnection>("hello");
-            DbCommand obj = new DbCommand(mockObj.Object,db);
+            DbCommand obj = new DbCommand(mockObj.Object);
             var stringWriter = new System.IO.StringWriter();
             Console.SetOut(stringWriter);
-            obj.Execute(db);
-            Assert.AreEqual(db+" is running\r\n", stringWriter.ToString());
+            obj.Execute();
+            Assert.AreEqual("DB is running\r\n", stringWriter.ToString());
         }
     }
 }
